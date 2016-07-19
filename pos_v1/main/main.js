c5e519934adf952b6bf203828b9c60429f44a87c
yhl221 <1727538058@qq.com>
2016-07-19 12:56:57 +0800

    second commit

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,58 @@
 'use strict';
+function printReceipt(tags) {
+  let itemCounts = buildItemsCount(tags);
+}
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function buildItemsCount(tags) {
+  let cartItems = [];
+  let allItems = loadAllItems();
+
+  for (let tag of tags) {
+    let splitedTag = tag.split('-');
+    let barcode = splitedTag[0];
+    let count = parseFloat(splitedTag[1] || 1);
+
+    let cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+    if (cartItem) {
+      cartItem.count++;
+    } else {
+      let item = allItems.find(item => item.barcode === barcode);
+      cartItems.push({item: item, count: count});
+    }
+  }
+  return cartItems;
+}
+
+
+function buildItemsSubtotal(cartItems) {
+  let itemSubtotals = [];
+  let allPromotions = loadPromotions();
+
+  for (let cartItem of cartItems) {
+    let itemPromotion = allPromotions[0].barcodes.find((itemPromotion) => {
+
+      return itemPromotion === cartItem.item.barcode
+    });
+    if (itemPromotion) {
+      itemSubtotals.push({cartItem: cartItem,
+        subtotal:(cartItem.count- parseInt(cartItem.count / 3)) * cartItem.item.price,
+        save:parseInt(cartItem.count / 3) * cartItem.item.price});
+    } else {
+      itemSubtotals.push({cartItem: cartItem, subtotal: cartItem.count * cartItem.item.price,save:0});
+    }
+  }
+
+  return itemSubtotals;
+}
+
+function buildItemsReceipt(itemSubtotals){
+  let itemReceipts;
+  let total=0,save=0;
+  for(let itemSubtotal of itemSubtotals) {
+    total+=itemSubtotal.subtotal;
+    save+=itemSubtotal.save;
+  }
+    itemReceipts={itemReceipt:itemSubtotals,total:total,save:save};
+  return itemReceipts;
+}
+
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -1,29 +1,106 @@
 'use strict';
 
 describe('pos', () => {
-  let inputs;
+         let inputs;
+         let itemCounts;
+         let itemSubtotals;
+        beforeEach(() => {
+            inputs = [
+                'ITEM000001',
+                'ITEM000001',
+                'ITEM000001',
+                'ITEM000001',
+                'ITEM000001',
+                'ITEM000003-2',
+                'ITEM000005',
+                'ITEM000005',
+                'ITEM000005'
+            ];
+        });
 
   beforeEach(() => {
-    inputs = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2',
-      'ITEM000005',
-      'ITEM000005',
-      'ITEM000005'
+    itemCounts = [
+      {
+        item: {
+          barcode: 'ITEM000001',
+          name: '雪碧',
+          unit: '瓶',
+          price: 3.00
+        },
+        count: 5
+      },
+      {
+        item: {
+          barcode: 'ITEM000003',
+          name: '荔枝',
+          unit: '斤',
+          price: 15.00
+        },
+        count: 2
+      },
+      {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        },
+        count: 3
+      }
     ];
   });
 
-  it('should print correct text', () => {
+  beforeEach(() => {
+    itemSubtotals = [
+      {
+        itemCount: {
+          item: {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+          },
+          count: 5
+        },
+        subtotal: 12.00,
+        save:3.00
+      },
+      {
+        itemCount: {
+          item: {
+            barcode: 'ITEM000003',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00
+          },
+          count: 2
+        },
+        subtotal: 30.00,
+        save:0.00
+      },
+      {
+        itemCount: {
+          item: {
+            barcode: 'ITEM000005',
+            name: '方便面',
+            unit: '袋',
+            price: 4.50
+          },
+          count: 3
+        },
+        subtotal: 9.00,
+        save:4.50
+      }
+    ];
+  });
 
-    spyOn(console, 'log');
+        it(' print receipts', () => {
 
-    printReceipt(inputs);
+            spyOn(console, 'log');
 
-    const expectText = `***<没钱赚商店>收据***
+            printReceipt(inputs);
+
+            const expectReceipts = `***<没钱赚商店>收据***
 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
 名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
@@ -32,6 +109,150 @@ describe('pos', () => {
 节省：7.50(元)
 **********************`;
 
-    expect(console.log).toHaveBeenCalledWith(expectText);
+            expect(console.log).toHaveBeenCalledWith(expectReceipts);
+        });
+
+    it(' print correct itemsCounts', () => {
+
+        let itemCounts;
+
+        itemCounts = buildItemsCount(inputs);
+
+        const expectitemCounts = [
+            {
+                item: {
+                    barcode: 'ITEM000001',
+                    name: '雪碧',
+                    unit: '瓶',
+                    price: 3.00
+                },
+                count: 5
+            },
+            {
+                item: {
+                    barcode: 'ITEM000003',
+                    name: '荔枝',
+                    unit: '斤',
+                    price: 15.00
+                },
+                count: 2
+            },
+            {
+                item: {
+                    barcode: 'ITEM000005',
+                    name: '方便面',
+                    unit: '袋',
+                    price: 4.50
+                },
+                count: 3
+            }
+        ];
+
+        expect(itemCounts).toEqual(expectitemCounts);
+    });
+
+  it(' print correct itemSubtotals', () => {
+
+    let itemSubtotals = buildItemsSubtotal(itemCounts);
+    const expectText = [
+      {
+        cartItem: {
+          item: {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+          },
+          count: 5
+      },
+        subtotal: 12.00,
+        save:3.00
+      },
+      {
+        cartItem: {
+          item: {
+            barcode: 'ITEM000003',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00
+          },
+          count: 2
+        },
+        subtotal: 30.00,
+        save:0.00
+      },
+      {
+        cartItem: {
+          item: {
+            barcode: 'ITEM000005',
+            name: '方便面',
+            unit: '袋',
+            price: 4.50
+          },
+          count: 3
+        },
+        subtotal: 9.00,
+        save:4.50
+      }
+    ];
+
+    expect(itemSubtotals).toEqual(expectText);
+  });
+
+  it(' print correct itemReceipts', ()=> {
+
+    let itemReceipts = buildItemsReceipt(itemSubtotals);
+    const expectText = {
+      itemReceipt: [
+        {
+          itemCount: {
+            item: {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00
+            },
+            count: 5
+          },
+          subtotal: 12.00,
+          save:3.00
+        },
+        {
+          itemCount: {
+            item: {
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00
+            },
+            count: 2
+          },
+          subtotal: 30.00,
+          save:0.00
+        },
+        {
+          itemCount: {
+            item: {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50
+            },
+            count: 3
+          },
+          subtotal: 9.00,
+          save:4.50
+        }
+      ],
+      total: 51.00,
+      save:7.50
+    };
+    expect(itemReceipts).toEqual(expectText);
   });
 });
+
+
+
+
+
+
